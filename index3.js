0b5654cbc150aad095436618b4b56e16b9edffeb
megafriday <megafriday@yahoo.co.jp>
2023-02-11 18:32:50 +0900

    change index3.js

--- index3.js
+++ index3.js
@@ -15,3 +15,72 @@
 
 console.log("--------------------------------------------------------");
 
+{
+	// 目標
+	// [
+	// 	{
+	// 		dateFrom: "2022-11-12",
+	// 		dateTo: "2022-11-15",
+	// 		index: 0,
+	// 		sample: { nameTitle: "sample1", unit: "dollar" },
+	// 	},
+	// 	{
+	// 		dateFrom: "2022-11-12",
+	// 		dateTo: "2022-11-15",
+	// 		index: 1,
+	// 		sample: { nameTitle: "sample1-2", unit: "dollar" },
+	// 	},
+	// 	{
+	// 		dateFrom: "2022-11-12",
+	// 		dateTo: "2022-11-16",
+	// 		index: 2,
+	// 		samples: { nameTitle: "sample2", unit: "yen" },
+	// 	},
+	// 	{
+	// 		dateFrom: "2022-11-12",
+	// 		dateTo: "2022-11-17",
+	// 		index: 3,
+	// 		samples: { nameTitle: "sample2", unit: "peso" },
+	// 	},
+	// ]
+
+
+	const array = [
+		{
+			dateFrom: "2022-11-12",
+			dateTo: "2022-11-15",
+			samples: [
+				{ nameTitle: "sample1", unit: "dollar" },
+				{ nameTitle: "sample1-2", unit: "dollar" },
+			],
+		},
+		{
+			dateFrom: "2022-11-12",
+			dateTo: "2022-11-16",
+			samples: [{ nameTitle: "sample2", unit: "yen" }],
+		},
+		{
+			dateFrom: "2022-11-12",
+			dateTo: "2022-11-17",
+			samples: [{ nameTitle: "sample2", unit: "peso" }],
+		},
+	];
+
+	let count = 0;
+	const result = array.reduce((previous, current) => {
+		const rows = current.samples.map((sample) => {
+			return {
+				dataFrom: `${current.dateFrom}`,
+				dataTo: `${current.dateTo}`,
+				index: count++,
+				sample: sample
+			};
+		});
+		return previous.concat(...rows);
+	}, []);
+
+
+	console.log(result);
+}
+
+console.log("--------------------------------------------------------");
